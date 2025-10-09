c4e5a8a14751928a10e72dbb19932cb732fc130d
brit-tiny <breakfast.at.brittinys@gmail.com>
2025-10-09 18:18:47 -0400

    Add SQL_basics.sql file

--- AssignmentFiles/SQL_basics.sql
+++ AssignmentFiles/SQL_basics.sql
@@ -8,23 +8,55 @@ USE coffeeshop_db;
 
 -- Q1) List all products (show product name and price), sorted by price descending.
 
+select name, price from products order by price desc;
+
 -- Q2) Show all customers who live in the city of 'Lihue'.
+
+select * from customers where city = "lihue";
 
 -- Q3) Return the first 5 orders by earliest order_datetime (order_id, order_datetime).
 
+select order_id, order_datetime from orders order by order_datetime asc limit 5;
+
 -- Q4) Find all products with the word 'Latte' in the name.
+
+select * from products where name like "latte";
 
 -- Q5) Show distinct payment methods used in the dataset.
 
+select distinct payment_method from orders;
+
 -- Q6) For each store, list its name and city/state (one row per store).
+
+select distinct name, city, state from stores;
 
 -- Q7) From orders, show order_id, status, and a computed column total_items
 --     that counts how many items are in each order.
 
+select order_datetime
+	o.order_id, 
+    o.status, 
+    sum(oi.quantity) as total_items 
+from 
+	orders as o 
+join 
+	order_items as oi on o.order_id =oi.order_id 
+group by 
+	o.order_id, o.status 
+order by 
+	o.order_id;
+
 -- Q8) Show orders placed on '2025-09-04' (any time that day).
 
+select *
+from orders
+where order_datetime >= '2025-09-04 00:00:00' and order_datetime < '2025-09-05 00:00:00';
+
 -- Q9) Return the top 3 most expensive products (price, name).
+
+select price, name from products order by price desc limit 3;
 
 -- Q10) Show customer full names as a single column 'customer_name'
 --      in the format "Last, First".
 
+select concat (last_name,', ', first_name) as customer_name from customers;
